87278a1946ff5fd9e011fa7635ebc59760766bf2
AVShutov <shutoff.alexey@gmail.com>
2021-09-17 14:17:12 +0300

    Clean up print

--- tasks/task02/triangle_area.py
+++ tasks/task02/triangle_area.py
@@ -3,25 +3,25 @@ import math
 def area_base_height():
     values = input("Enter base and height: ").split(" ")
     if len(values) != 2 or not all(i.isdigit() for i in values) or not all(int(i) >= 0 for i in values):
-        print(f"\nCheck values (only two positive numbers greater than zero are allowed).\n")
+        print("Check values (only two positive numbers greater than zero are allowed).")
         return area_base_height()
     else:
         base = int(values[0])
         height = int(values[1])
         return (height * base) / 2
 
-def area_2sides_angle():
+def area_by_sides_and_angle():
     values = input("Enter 2 sides and angle(degrees) between them: ").split(" ")
     if len(values) != 3 or not all(i.isdigit() for i in values) or not all(int(i) >= 0 for i in values):
-        print(f"\nCheck values (only three positive numbers greater than zero are allowed).\n")
-        return area_2sides_angle()
+        print("Check values (only three positive numbers greater than zero are allowed).")
+        return area_by_sides_and_angle()
     else:
         side1 = int(values[0])
         side2 = int(values[1])
         angle = int(values[2])
         return (side1 * side2 * math.sin(math.radians(angle))) / 2
 
-print ("Welcome to the triangle area calculation tool.\n")
+print ("Welcome to the triangle area calculation tool.")
 
 menu = '''Menu:
 1. Calculate triangle area by base and height
@@ -35,11 +35,11 @@ while menu_item != 3:
     menu_item = int(input("Enter menu item number: "))
     if menu_item == 1:
         area = area_base_height()
-        print(f"\nTriangle Area is:  {area:.2f}\n")
+        print(f"Triangle Area is:  {area:.2f}")
     elif menu_item == 2:
-        area = area_2sides_angle()
-        print(f"\nTriangle Area is:  {area:.2f}\n")
+        area = area_by_sides_and_angle()
+        print(f"Triangle Area is:  {area:.2f}")
     elif menu_item == 3:
         break
     else:
-        print("\nMenu item number is not correct\n")
+        print("Menu item number is not correct")
