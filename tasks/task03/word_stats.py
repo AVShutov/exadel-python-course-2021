6be7550fec5f6124799ac513894fcde97ec56928
Aliaksei SHutau <ashutau@exadel.com>
2021-09-20 12:35:07 +0300

    Use functions

--- tasks/task03/word_stats.py
+++ tasks/task03/word_stats.py
@@ -9,14 +9,19 @@ texts = [
 word_stats = {}
 line = 0
 
+def word_statistics(word_stats):
+    if(word not in word_stats):
+        word_stats[word] = {'word_count': 1,'line_number': line}
+    else:
+        word_stats[word]['word_count'] = word_stats[word]['word_count']+1
+
+def print_stats(word_stats):
+    print("word\tcount\tfirst line")
+    for i in word_stats:
+        print(f"{i}\t{word_stats[i]['word_count']}\t{word_stats[i]['line_number']}")
+
 for text in texts:
     for word in re.sub('([^A-z^\s])+', '', text).lower().split():
-        if(word not in  word_stats):
-            word_stats[word] = {'word_count': 1,'line_number': line}
-        else:
-            word_stats[word]['word_count'] = word_stats[word]['word_count']+1
+        word_statistics(word_stats)
     line += 1
-
-print("word\tcount\tfirst line")
-for i in word_stats:
-    print(f"{i}\t{word_stats[i]['word_count']}\t{word_stats[i]['line_number']}")
+print_stats(word_stats)
