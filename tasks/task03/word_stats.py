78ca61700a96dadce22909b37cabf926880c851b
Aliaksei SHutau <ashutau@exadel.com>
2021-09-21 11:16:04 +0300

    Add enumerate func

--- tasks/task03/word_stats.py
+++ tasks/task03/word_stats.py
@@ -7,22 +7,18 @@ texts = [
 ]
 
 word_stats = {}
-line = 0
-
-def word_statistics(word_stats):
-    if(word not in word_stats):
-        word_stats[word] = {'word_count': 1,'line_number': line}
-    else:
-        word_stats[word]['word_count'] = word_stats[word]['word_count']+1
 
 def print_stats(word_stats):
     print("word\tcount\tfirst line")
     for i in word_stats:
         print(f"{i}\t{word_stats[i]['word_count']}\t{word_stats[i]['line_number']}")
 
-for text in texts:
-   word_list = re.sub('([^A-z^\s])+', '', text).lower().split()
+for line, text in enumerate(texts):
+    word_list = re.sub('([^A-z^\s])+', '', text).lower().split()
     for word in word_list:
-        word_statistics(word_stats)
-    line += 1
+        if (word not in word_stats):
+            word_stats[word] = {'word_count': 1,'line_number': line}
+        else:
+            word_stats[word]['word_count'] += 1
+
 print_stats(word_stats)
